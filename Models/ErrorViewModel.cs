ee420accd2ba5bb885e56e718e57419281abe764
HabtamuArgeta <argetahabtamu8@gmail.com>
2024-11-23 17:26:32 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nokosha.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
